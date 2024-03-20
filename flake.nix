f3abb3a2fc768eed655203c10c0a793e2681ac2e
YourUsername <zimulajan0@gmail.com>
2024-03-20 09:30:24 +0000

    solid dependency management

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "ComdaO project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            direnv
+            poetry
+          ];
+          shellHook = ''
+            export PATH=$PWD/.venv/bin:$PATH
+            poetry install
+          '';
+        };
+      }
+    );
+}
