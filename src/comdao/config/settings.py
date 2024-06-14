4ebb6f9d3eaced3a682b1a37a120811301d1af66
PsicoThePato <jmomcontato@gmail.com>
2024-06-14 05:13:53 -0300

    ROLE_ID parameter

--- src/comdao/config/settings.py
+++ src/comdao/config/settings.py
@@ -12,6 +12,7 @@ class DiscordParams(BaseSettings):
     GUILD_ID: int
     REQUEST_CHANNEL_ID: int
     NOMINATOR_CHANNEL_ID: int
+    ROLE_ID: int
 
     class Config:
         env_prefix = "DISCORD_"
@@ -31,7 +32,6 @@ MINUTES = 60 * 60
 HOURS = MINUTES * 60
 DAYS = HOURS * 24
 ROLE_NAME = "dao-member"
-ROLE_ID = 1225528654964985986
 #NODE_URL = "wss://testnet-commune-api-node-0.communeai.net"  # "wss://commune.api.onfinality.io/public-ws"
 USE_TESTNET = False
 MODULE_SUBMISSION_DELAY = 3600
@@ -39,4 +39,5 @@ INTENTS = discord.Intents.all()
 BOT = commands.Bot(command_prefix="/", intents=INTENTS)
 MNEMONIC = Subspace().MNEMONIC # type: ignore
 DISCORD_PARAMS = DiscordParams() # type: ignore
+ROLE_ID = DISCORD_PARAMS.ROLE_ID
 MAXIMUM_VOTING_AGE = DAYS * 1
